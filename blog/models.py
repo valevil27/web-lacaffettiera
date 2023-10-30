81967115e318646765152bd3b2deefdbd5e39c4b
valero <evalerovilella@gmail.com>
2023-10-30 11:01:15 +0100

    Created blog app Created Category and Post models

---
+++ blog/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField("Nombre", max_length=100)
+    created = models.DateTimeField("Fecha de Creación", auto_now_add=True)
+    updated = models.DateTimeField("Última Actualización", auto_now=True)
+    
+    class Meta:
+        verbose_name = "categoría"
+        verbose_name_plural = "categorías"
+        ordering = ["name"]
+        
+    def __str__(self) -> str:
+        return self.name.title()
+
+class Post(models.Model):
+    title= models.CharField("Título", max_length=100)
+
+    content= models.TextField("Contenido")
+    published= models.DateTimeField("Fecha de Publicación", default = timezone.now)
+    image= models.ImageField("Imagen", upload_to="blog", null=True, blank=True)
+    # OtO
+    author=models.ForeignKey(User, models.CASCADE, verbose_name="Autor")
+    categories = models.ManyToManyField(Category, "Categorías")
+    created = models.DateTimeField("Fecha de Creación", auto_now_add=True)
+    updated = models.DateTimeField("Última Actualización", auto_now=True)
+    
+    class Meta:
+        verbose_name = "categoría"
+        verbose_name_plural = "categorías"
+        ordering = ["-published"]
+        
+    def __str__(self) -> str:
+        return self.title.title()
