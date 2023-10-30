59f44701ba5e53114e3780e071fca62d76c053e1
valero <evalerovilella@gmail.com>
2023-10-30 12:00:10 +0100

    Admin Pannel custom for Posts

--- blog/admin.py
+++ blog/admin.py
@@ -6,6 +6,17 @@ class CategoryAdmin(admin.ModelAdmin):
     readonly_fields = ("created", "updated")
 admin.site.register(Category, CategoryAdmin)
 
-class BlogAdmin(admin.ModelAdmin):
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published', "post_categories")
+    list_filter = ("categories__name", )
+    ordering = ('author', '-published')
     readonly_fields = ("created", "updated")
-admin.site.register(Post, BlogAdmin)
+    search_fields = ("title","author__username", "categories__name")
+    date_hierarchy = "published"
+    
+    def post_categories(self, obj: Post):
+        return ", ".join([c.name for c in obj.categories.all().order_by("name")])
+    
+    post_categories.short_description = "Categorías"
+
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField("Nombre", max_length=100)
+    name = models.CharField("Categoría", max_length=100)
     created = models.DateTimeField("Fecha de Creación", auto_now_add=True)
     updated = models.DateTimeField("Última Actualización", auto_now=True)
     
