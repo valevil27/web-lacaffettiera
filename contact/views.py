319d7cb08a652c6734a81c2ea13463eee36c5a75
valero <evalerovilella@gmail.com>
2023-10-31 11:19:52 +0100

    initial contact form

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .forms import ContactForm
 
 # Create your views here.
 def contact(request):
-    return render(request, "contact/contact.html")
+    form = ContactForm()
+    return render(request, "contact/contact.html", {"form":form})
